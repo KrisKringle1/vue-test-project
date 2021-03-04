16a464281e097325c20fde02879d97d2bc7924dc
KrisKringle1 <kmringler@gmail.com>
2021-03-03 19:26:13 -0800

    add / remove using methods

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ new Vue({
     el: '#root',
     data() {
         return{
-            message: "this is a message",
-            counter: 0
+            fruits: [
+                'apple',
+                'oranges',
+                'grapefruits'
+            ],
+            currentFruit: ''
+        }
+    },
+    methods: {
+        addFruit: function(){
+            this.fruits.push(this.currentFruit)
+            this.currentFruit = ''; 
+        },
+        removeFruit: function(index){
+            this.fruits.splice(index, 1)
         }
     }
 });

94b009a2ec5d250b98baee70488f7845ec650843
KrisKringle1 <kmringler@gmail.com>
2021-03-03 19:57:06 -0800

    computed

---
+++ computed.js
@@ -0,0 +1,39 @@
+new Vue({
+    el: "#root",
+    data() {
+        return {
+            columns: ['name', 'stars'],
+            stores: [
+                {name: 'Chipotle', stars: 5.0},
+                {name: 'Pizza Hut', stars: 3.5},
+                {name: 'Carls Jr', stars: 3.1},
+                {name: 'Chic Fil A', stars: 5.0},
+                {name: 'McDonalds', stars: 4.2},
+                {name: 'Burger King', stars: 1.0},
+                {name: 'Wendys', stars: 3.2},
+                {name: 'Whataburger', stars: 5.0},
+                {name: 'Jack in the Box', stars: 2.0}
+            ],
+            searchText: '',
+        }
+    },
+    methods: {
+        sortName(){
+            this.stores.sort((a,b) =>  a.name > b.name ? 1 : -1)
+        },
+        sortLowest(){
+            this.stores.sort((a,b) => a.stars >  b.stars ? 1 : -1)
+
+        },
+        sortHighest(){
+            this.stores.sort((a,b) => a.stars < b.stars ? 1 : -1)
+
+        }
+    },
+    computed: {
+        searchedStores (){
+            const filter = new RegExp(this.searchText, 'i');
+            return this.stores.filter(el => el.name.match(filter))
+        }
+    }
+})
